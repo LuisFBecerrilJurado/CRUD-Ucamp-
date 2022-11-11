3a1c8272b08fe0dac06cd9f1dbc97fd1d93f1633
Luis Francisco Becerril Jurado <99098643+LuisFBecerrilJurado@users.noreply.github.com>
2022-11-11 01:42:03 -0600

    [Update] Cambiando el nombre de funcion readTask => displayTaskpara renderizar las tareas

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import { addTask } from "./components/addTask.js";
-import { readTask } from "./components/readTask.js";
+import { displayTask } from "./components/readTask.js";
 
 const btn = document.querySelector("[data-form-btn]");
 
 btn.addEventListener("click", addTask);
-readTask();
+displayTask();
